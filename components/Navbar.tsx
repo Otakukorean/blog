82d1c9c61b88ed3fa8dc6c0ed03c73518542e4f8
Hotturtle1 <ahmadmustafa287@yahoo.com>
2024-06-05 00:48:18 +0300

    mobile2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -45,16 +45,14 @@ const Navbar = () => {
             <h1 onClick={() => router.push('/')} className='text-white text-xl font-bold  first-letter:text-[#39FCF2] first-letter:pr-[1px] first-letter:text-2xl cursor-pointer'>Blogna</h1>
         </div>
         <div className='flex items-center justify-center gap-4 '>
-        
-            {
-               width > 600 ? (
-                    <Button onClick={() => searchModal.onOpen()} style={{direction:'rtl'}} className='inline-flex items-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50  border-input hover:bg-borderGray hover:text-white/80 px-2 py-2 relative h-8 w-full justify-start rounded-[0.5rem] bg-borderGray text-sm font-normal text-muted-foreground shadow-none pr-12 sm:w-40 '>
+
+                 <Button onClick={() => searchModal.onOpen()} style={{direction:'rtl'}} className='hidden md:inline-flex  items-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50  border-input hover:bg-borderGray hover:text-white/80 px-2 py-2 relative h-8 w-full justify-start rounded-[0.5rem] bg-borderGray text-sm font-normal text-muted-foreground shadow-none pr-12 sm:w-40 '>
                     <span className={cn(font.className)}>ابحث عن مقال</span>
                 </Button>
-                ) : (
-                    <CiSearch onClick={() => searchModal.onOpen()} className='transition-all ease text-white w-6 h-6 cursor-pointer hover:text-white/75'/>
-                )
-            }
+          
+                <CiSearch onClick={() => searchModal.onOpen()} className='md:hidden transition-all ease text-white w-6 h-6 cursor-pointer hover:text-white/75'/>
+           
+        
             {user?.role === 'ADMIN' &&<Link href={'/write'}><FaRegPenToSquare  className='transition-all ease text-white w-5 h-5 cursor-pointer hover:text-white/75' /></Link> }
             {user?.role === 'ADMIN' &&<TagsButton /> }
             {!user &&             <Button variant={'white'} className={cn(font.className)} onClick={() => loginmodal.onOpen()}>تسجيل الدخول</Button>}       
